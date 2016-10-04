cb9a9f1b638c606b03a0d3401d92bd31cb4b17ac
Camilo Carromeu <camilo@carromeu.com>
2016-10-04 10:05:46 -0400

    Exemplo de arquivo para database migration.

---
+++ update/db/20161004095701.sql
@@ -0,0 +1,8 @@
+
+-- Sample of file name format: YYYYMMDDHHIIXX.sql
+-- YYYY: Year with 4 digits (e.g., '2016')
+-- MM: Month with 2 digits (e.g., '09' for september)
+-- DD: Day of month with 2 digits (e.g., '04')
+-- HH: Hour of day in 24h format with 2 digits (e.g., '13' for 1pm)
+-- II: Minutes with 2 digits (e.g., '57')
+-- XX: A sequence starting in '01' (e.g., '03' for third file commited in same minute)
