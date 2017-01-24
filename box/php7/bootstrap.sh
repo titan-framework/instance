b27ceada7b044209e7fad1361aa13f630f08e392
Camilo Carromeu <camilo@carromeu.com>
2017-01-24 09:08:01 -0300

    Tenta carregar o autoload do Composer, caso esteja habilitado.

--- box/php7/bootstrap.sh
+++ box/php7/bootstrap.sh
@@ -28,7 +28,7 @@ echo "Install a lot of dependencies..."
 echo "postfix postfix/mailname string localhost" | debconf-set-selections
 echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
 
-apt-get install -y antiword aptitude build-essential bzip2 curl git libav-tools locales locate mailutils nginx ntpdate openjdk-9-jre php7.0-fpm php7.0-cli php7.0-curl php7.0-dev php7.0-gd php-imagick php7.0-ldap php7.0-mcrypt php-pear php7.0-pgsql php7.0-sqlite postfix subversion xpdf-utils unzip vim
+apt-get install -y antiword aptitude build-essential bzip2 curl git libav-tools locales locate mailutils nginx ntpdate openjdk-9-jre php7.0-fpm php7.0-cli php7.0-curl php7.0-dev php7.0-gd php-imagick php7.0-ldap php7.0-mbstring php7.0-mcrypt php-pear php7.0-pgsql php7.0-sqlite postfix subversion xpdf-utils unzip vim
 
 echo "Done!"
 
--- titan.php
+++ titan.php
@@ -5,7 +5,7 @@ $file = 'configure/titan.xml';
 if (!file_exists ($file))
 {
 	header ('HTTP/1.1 500 Internal Server Error');
-	
+
 	die ('Configuration file [configure/titan.xml] not found!');
 }
 
@@ -20,11 +20,11 @@ if (!isset ($match [1][0]))
 	$regTag = '/core-path="(.*?)"/s';
 
 	preg_match_all ($regTag, $xml, $match);
-	
+
 	if (!isset ($match [1][0]))
 	{
 		header ('HTTP/1.1 500 Internal Server Error');
-		
+
 		die ('Attribute [core-path] must be setted at configuration file [configure/titan.xml].');
 	}
 }
@@ -34,10 +34,20 @@ $corePath = $match [1][0];
 if (!file_exists ($corePath .'switch.php'))
 {
 	header ('HTTP/1.1 500 Internal Server Error');
-	
+
 	die ('Titan core has not found in path ['. $corePath .'] specified at configuration file [configure/titan.xml].');
 }
 
-require 'vendor/autoload.php';
+if (file_exists ('composer.json'))
+{
+	if (file_exists ('vendor/autoload.php'))
+		require 'vendor/autoload.php';
+	else
+	{
+		header ('HTTP/1.1 500 Internal Server Error');
+
+		die ('Please, install dependencies using Composer (http://getcomposer.org/): "composer install --no-dev"!');
+	}
+}
 
 require $corePath .'switch.php';
